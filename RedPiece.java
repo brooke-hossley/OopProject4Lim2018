05f2aff67cc076aa65213b60135bc184c8457241
chr1sadams <37448132+chr1sadams@users.noreply.github.com>
2018-03-21 18:52:23 -0400

    Update RedPiece.java

--- RedPiece.java
+++ RedPiece.java
@@ -5,10 +5,19 @@ package OopProject4Lim2018;
  * @author (your name here)
  * @version (version number or date here)
  */
-public class RedPiece extends Pieces
+public class RedPiece extends Piece
 {
-    public RedPiece()
+
+    /**
+     * Constructs a RedPiece with orientation passed in
+     * 
+     * @param orientation the initial orientation of the piece
+     * @param rotatable whether the piece can be rotated. Should be false if piece doesn't start as a "?"
+     */
+    public RedPiece(Orientation orientation, boolean rotatable)
     {
-        
+        super(orientation, rotatable, false, true);
     }
+    
+    
 }
