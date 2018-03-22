99a9dd9cd7230b53c6810d0d864169e70de17df1
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-03-22 16:53:14 -0400

    current board with images as buttons?
    not sure what we are gonna do with this but hey,

--- LaserMazeBoardPanel.java
+++ LaserMazeBoardPanel.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 class LaserMazeBoardPanel extends JPanel implements MouseListener {
     private Image board;
     private Image purpleMirror;
@@ -29,6 +31,33 @@ class LaserMazeBoardPanel extends JPanel implements MouseListener {
         frame.setTitle("Lase Maze");
         CircleButton fireButton = new CircleButton("FIRE");
         fireButton.setBounds(595,490,100,100);
+        BufferedImage buttonImage1;
+        BufferedImage greenMirror;
+        JButton button1;
+        JButton button2;
+        try
+        {
+
+            buttonImage1 = ImageIO.read(new File("PurpleMirror.JPG"));
+            greenMirror = ImageIO.read(new File("GreenMirror.JPG"));
+            button1 = new JButton(new ImageIcon(buttonImage1));
+            button1.setBorder(BorderFactory.createEmptyBorder());
+            button1.setContentAreaFilled(false);
+            button1.setBounds(55,350,90,90);
+            button1.setBorder(BorderFactory.createEmptyBorder());
+            button1.setContentAreaFilled(false); 
+            panel.add(button1);
+            
+            button2 = new JButton(new ImageIcon(greenMirror));
+            button2.setBorder(BorderFactory.createEmptyBorder());
+            button2.setContentAreaFilled(false);
+            button2.setBounds(550,50,175,175);
+            button2.setBorder(BorderFactory.createEmptyBorder());
+            button2.setContentAreaFilled(false);
+            panel.add(button2);
+        }
+        catch(Exception e){}
+
         panel.add(fireButton);
         frame.pack();
         frame.setVisible(true);
@@ -43,7 +72,6 @@ class LaserMazeBoardPanel extends JPanel implements MouseListener {
     public void mouseExited(MouseEvent me) { }
 
     public void mouseClicked(MouseEvent me) { }
-
 
     public void paintComponent(Graphics g) {
         g.drawImage(board, 0, 0, null);
