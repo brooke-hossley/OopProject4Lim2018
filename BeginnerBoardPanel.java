26f035aa286b69e447788a864229b7da3a7db773
aronca <31697397+aronca@users.noreply.github.com>
2018-03-25 23:00:59 -0400

    Rotation nation

--- BeginnerBoardPanel.java
+++ BeginnerBoardPanel.java
@@ -12,43 +12,69 @@ import javax.imageio.*;
  */
 public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMotionListener
 {
-    /** position of the rectangle */
+    // position of the movable purple mirror
     private int x, y;
 
-    /** am I currently dragging the rectangle? */
+    // am I currently dragging the movable mirror? 
     private boolean dragging;
-    
-    private boolean rotating;
 
-    /** remember mouse "grab point" in the rectangle */
+    // remember mouse "grab point" in the movable mirror 
     private int xOffset, yOffset;
 
-    /** icon size */
+    // icon size 
     public static final int MIRROR_SIZE = 80;
 
+    //all the images we're going to use
     private static Image board;
-    private Image redLaser, redQuestionMark, purpleMirror, purpleMirrorTarget, purpleQuestionMark;
-
+    private Image[] redLasers,  purpleMirrors;
+    private Image redQuestionMark, purpleMirrorTarget, purpleQuestionMark;
+    
+    //what the current image being used by game is for laser and moveable mirror
+    private Image ourLaser, ourMovablePurple;
+    
+    //array index ourLaser and ourMovablePurple currently are
+    private int redIndex, purpleIndex;
+    
+    
+    
     public BeginnerBoardPanel() {
+        
+        //set images we start with on board
         board = new ImageIcon("Images\\Board.PNG").getImage();
-        redLaser = new ImageIcon("Images\\LaserUp.JPG").getImage();
         redQuestionMark = new ImageIcon("Images\\RedLaserQuestion.JPG").getImage();
-        purpleMirror = new ImageIcon("Images\\PurpleMirrorLeft.JPG").getImage();
         purpleMirrorTarget = new ImageIcon("Images\\PurpleMirrorWithTargetDown.JPG").getImage();
         purpleQuestionMark = new ImageIcon("Images\\PurpleQuestionMark.JPG").getImage();
-
-        //setBackground( Color.WHITE );
-        //setOpaque(true);
+        ourLaser = redQuestionMark;
+        ourMovablePurple = purpleQuestionMark;
+        //these don't really matter yet
+        redIndex = 0;
+        purpleIndex = 0;
+        
+        // initial position of our movable mirror
+        x = 602;
+        y = 75;
+        
+        //load in purple and red piece images for rotation later
+        purpleMirrors = new Image[4];
+        purpleMirrors[0] = new ImageIcon("Images\\PurpleMirrorUp.JPG").getImage();
+        purpleMirrors[1] = new ImageIcon("Images\\PurpleMirrorRight.JPG").getImage();
+        purpleMirrors[2] = new ImageIcon("Images\\PurpleMirrorDown.JPG").getImage();
+        purpleMirrors[3] = new ImageIcon("Images\\PurpleMirrorLeft.JPG").getImage();
+        
+        redLasers = new Image[4];
+        redLasers[0] = new ImageIcon("Images\\LaserUp.JPG").getImage();
+        redLasers[1] = new ImageIcon("Images\\LaserRight.JPG").getImage();
+        redLasers[2] = new ImageIcon("Images\\LaserDown.JPG").getImage();
+        redLasers[3] = new ImageIcon("Images\\LaserLeft.JPG").getImage();
+        
+        //set window to correct size
         Dimension size = new Dimension(board.getWidth(null), board.getHeight(null));
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
         setSize(size);
         setLayout(null);
-        // initial position of our draggable rectangle
-        x = 602;
-        y = 75;
-
+        
         addMouseListener(this);
         addMouseMotionListener(this);
     }
@@ -61,20 +87,58 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
      */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        //these will stay same because hardcoded
         g.drawImage(board, 0, 0, null);
         g.drawImage(purpleMirrorTarget, 448, 69, null);
-        g.drawImage(purpleMirror, 65, 356, null);
-        g.drawImage(redQuestionMark, 65, 452, null);
-        g.drawImage(purpleQuestionMark, x, y, null);
+        g.drawImage(purpleMirrors[3], 65, 356, null);
+        
+        //these can both rotate and purple can be dragged
+        g.drawImage(ourLaser, 65, 452, null);
+        g.drawImage(ourMovablePurple, x, y, null);
 
     }
 
-    public void mouseClicked(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+        
+        //if clicked on laser rotate it
+        if (e.getX() >= 65 && e.getX() <= 65 + MIRROR_SIZE &&
+                    e.getY() >= 452 && e.getY() <= 452 + MIRROR_SIZE) {
+            if (ourLaser == redQuestionMark || ourLaser == redLasers[3]) {
+                ourLaser = redLasers[0];
+                redIndex = 0;
+                repaint();
+            }
+            else {
+                redIndex++; 
+                ourLaser = redLasers[redIndex];
+                repaint();
+            }
+        }
+        
+        //if clicked on movable mirror rotate it
+        else if (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
+                    e.getY() >= y && e.getY() <= y + MIRROR_SIZE) {
+            if (ourMovablePurple == purpleQuestionMark || ourMovablePurple == purpleMirrors[3]) {
+                ourMovablePurple = purpleMirrors[0];
+                purpleIndex = 0;
+                repaint();
+            }
+            else {
+                purpleIndex++; 
+                ourMovablePurple = purpleMirrors[purpleIndex];
+                repaint();
+            }
+        }
+    }
 
     public void mouseEntered(MouseEvent e) {}
 
     public void mouseExited(MouseEvent e) {}
-
+    
+    public void mouseMoved(MouseEvent e) {}
+        
+    public void mouseReleased(MouseEvent e) {}
+    
     /** 
      * determine if the mouse was pressed in the bounds of
      * our movable image.
@@ -82,17 +146,10 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
      * @param e mouse event information
      */
     public void mousePressed(MouseEvent e) {
-
-        //////////////definitely gonna have to add to this for rotation\\\\\\\\\\\\\\
-
         dragging = (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
                     e.getY() >= y && e.getY() <= y + MIRROR_SIZE);
         xOffset = e.getX() - x;
         yOffset = e.getY() - y;
-    }
-
-    public void mouseReleased(MouseEvent e) {
-        //repaint();
     }
 
     public void mouseDragged(MouseEvent e) {
@@ -106,19 +163,6 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
             repaint();
         }
     }
-
-    public void mouseMoved(MouseEvent e) {
-
-        // if (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
-        // e.getY() >= y && e.getY() <= y + MIRROR_SIZE) {
-            //rectColor = Color.red;
-        // }
-        // else {
-            // rectColor = Color.black;
-        // }
-        // repaint();
-    }
-
     
     private static void createAndShowGUI() {
 
