b3a45e83db09d465095d13a460381714c4d2bd6a
Patrick Barber <37307782+patrick-barber@users.noreply.github.com>
2018-03-26 12:51:36 -0400

    Added fire button functionality
    the fire button now draws the laser line of the solution, does not draw incorrect solution lines.. yet. Might want to consider adding a timer so the line clears it self after a few seconds.. and adding in the lines that could be drawn if the incorrect orientation of mirrors is made. Also some type of dialog box that tells the user they are correct or incorrect.

--- BeginnerBoardPanel.java
+++ BeginnerBoardPanel.java
@@ -4,13 +4,16 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
+import java.awt.geom.Line2D;
+
+///////////////////////////////////////////////////////////////////////////////////// 
 /**
- * Write a description of class BeginnerBoardPanel here.
+ * Creates the board with draggable images and rotable mirrors.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Alissa, Patrick, Chris, Hieu, Chris)
+ * @version (3/26/18)
  */
-public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMotionListener
+public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMotionListener , ActionListener
 {
     // position of the movable purple mirror
     private int x, y;
@@ -28,17 +31,16 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
     private static Image board;
     private Image[] redLasers,  purpleMirrors;
     private Image redQuestionMark, purpleMirrorTarget, purpleQuestionMark;
-    
+
     //what the current image being used by game is for laser and moveable mirror
     private Image ourLaser, ourMovablePurple;
-    
+
     //array index ourLaser and ourMovablePurple currently are
     private int redIndex, purpleIndex;
-    
-    
-    
+
+    private CircleButton fireButton;
+    private boolean draw = false;
     public BeginnerBoardPanel() {
-        
         //set images we start with on board
         board = new ImageIcon("Images\\Board.PNG").getImage();
         redQuestionMark = new ImageIcon("Images\\RedLaserQuestion.JPG").getImage();
@@ -49,11 +51,11 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         //these don't really matter yet
         redIndex = 0;
         purpleIndex = 0;
-        
+
         // initial position of our movable mirror
         x = 602;
         y = 75;
-        
+
         //load in purple and red piece images for rotation later
         purpleMirrors = new Image[4];
         purpleMirrors[0] = new ImageIcon("Images\\PurpleMirrorUp.JPG").getImage();
@@ -74,9 +76,14 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         setMaximumSize(size);
         setSize(size);
         setLayout(null);
-        
+
         addMouseListener(this);
         addMouseMotionListener(this);
+
+        fireButton = new CircleButton("FIRE");
+        fireButton.setBounds(595,490,100,100);
+        add(fireButton);
+        fireButton.addActionListener(this);
     }
 
     /**
@@ -91,18 +98,34 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         g.drawImage(board, 0, 0, null);
         g.drawImage(purpleMirrorTarget, 448, 69, null);
         g.drawImage(purpleMirrors[3], 65, 356, null);
-        
+
         //these can both rotate and purple can be dragged
         g.drawImage(ourLaser, 65, 452, null);
         g.drawImage(ourMovablePurple, x, y, null);
 
+        if (draw) {
+            g.setColor(Color.BLUE);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(10));
+            g.drawLine(100,455,100,410);
+            g.drawLine(100,410,480,410);
+            g.drawLine(480,410,480,130);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+    //this is what gets called when the fire button is pressed
+        draw = true;
+        repaint();
     }
 
     public void mouseClicked(MouseEvent e) {
-        
+
         //if clicked on laser rotate it
         if (e.getX() >= 65 && e.getX() <= 65 + MIRROR_SIZE &&
-                    e.getY() >= 452 && e.getY() <= 452 + MIRROR_SIZE) {
+        e.getY() >= 452 && e.getY() <= 452 + MIRROR_SIZE) {
             if (ourLaser == redQuestionMark || ourLaser == redLasers[3]) {
                 ourLaser = redLasers[0];
                 redIndex = 0;
@@ -114,10 +137,10 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
                 repaint();
             }
         }
-        
+
         //if clicked on movable mirror rotate it
         else if (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
-                    e.getY() >= y && e.getY() <= y + MIRROR_SIZE) {
+        e.getY() >= y && e.getY() <= y + MIRROR_SIZE) {
             if (ourMovablePurple == purpleQuestionMark || ourMovablePurple == purpleMirrors[3]) {
                 ourMovablePurple = purpleMirrors[0];
                 purpleIndex = 0;
@@ -134,11 +157,11 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
     public void mouseEntered(MouseEvent e) {}
 
     public void mouseExited(MouseEvent e) {}
-    
+
     public void mouseMoved(MouseEvent e) {}
-        
+
     public void mouseReleased(MouseEvent e) {}
-    
+
     /** 
      * determine if the mouse was pressed in the bounds of
      * our movable image.
@@ -147,40 +170,35 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
      */
     public void mousePressed(MouseEvent e) {
         dragging = (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
-                    e.getY() >= y && e.getY() <= y + MIRROR_SIZE);
+            e.getY() >= y && e.getY() <= y + MIRROR_SIZE);
         xOffset = e.getX() - x;
         yOffset = e.getY() - y;
     }
 
     public void mouseDragged(MouseEvent e) {
-        
+
         //Since x and y are reset here
         //Will need to add functionality for making sure mirror goes into actual board spot
-        
+
         if (dragging) {
             x = e.getX() - xOffset;
             y = e.getY() - yOffset;
             repaint();
         }
     }
-    
-    private static void createAndShowGUI() {
 
+    private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("BeginnerBoardPanel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BeginnerBoardPanel panel = new BeginnerBoardPanel();
         frame.getContentPane().add(panel);
-        //panel.addImage(board);
-        CircleButton fireButton = new CircleButton("FIRE");
-        fireButton.setBounds(595,490,100,100);
-        panel.add(fireButton);
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
