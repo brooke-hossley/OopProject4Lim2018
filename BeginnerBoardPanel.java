6489098aa940ae0f6b3447eea56847a7d55028c3
brooke-hossley <31697381+brooke-hossley@users.noreply.github.com>
2018-04-07 14:27:21 -0400

    Add files via upload

--- BeginnerBoardPanel.java
+++ BeginnerBoardPanel.java
@@ -12,12 +12,13 @@ import java.awt.geom.Line2D;
  * @author (Alissa, Patrick, Chris, Hieu, Chris)
  * @version (3/26/18)
  */
-public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMotionListener , ActionListener
+public class BeginnerBoardPanel extends JPanel implements 
+MouseListener, MouseMotionListener , ActionListener
 {
     //used to drag to correct spot
     private BoardLocations locations;
     private Point mirrorPoints[], sidePanel;
-    
+
     // position of the movable purple mirror
     private int x, y;
 
@@ -43,7 +44,7 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
 
     private CircleButton fireButton;
     private boolean draw = false;
-    
+
     public BeginnerBoardPanel() {
         locations = new BoardLocations();
         sidePanel = new Point(602, 75);
@@ -52,13 +53,16 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         mirrorPoints[1] = locations.locationPoints[0][3];
         mirrorPoints[2] = locations.locationPoints[0][4];
         mirrorPoints[3] = sidePanel;
-        
+
         //set images we start with on board
         String dir = "Images\\";
         board = new ImageIcon(dir + "Board.PNG").getImage();
-        redQuestionMark = new ImageIcon(dir + "RedLaserQuestion.JPG").getImage();
-        purpleMirrorTarget = new ImageIcon(dir + "PurpleMirrorWithTargetDown.JPG").getImage();
-        purpleQuestionMark = new ImageIcon(dir + "PurpleQuestionMark.JPG").getImage();
+        redQuestionMark = new ImageIcon(dir + 
+            "RedLaserQuestion.JPG").getImage();
+        purpleMirrorTarget = new ImageIcon(dir + 
+            "PurpleMirrorWithTargetDown.JPG").getImage();
+        purpleQuestionMark = new ImageIcon(dir + 
+            "PurpleQuestionMark.JPG").getImage();
         ourLaser = redQuestionMark;
         ourMovablePurple = purpleQuestionMark;
 
@@ -68,19 +72,28 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
 
         //load in purple and red piece images for rotation later
         purpleMirrors = new Image[4];
-        purpleMirrors[0] = new ImageIcon(dir + "PurpleMirrorUp.JPG").getImage();
-        purpleMirrors[1] = new ImageIcon(dir + "PurpleMirrorRight.JPG").getImage();
-        purpleMirrors[2] = new ImageIcon(dir + "PurpleMirrorDown.JPG").getImage();
-        purpleMirrors[3] = new ImageIcon(dir + "PurpleMirrorLeft.JPG").getImage();
-        
+        purpleMirrors[0] = new ImageIcon(dir + 
+            "PurpleMirrorUp.JPG").getImage();
+        purpleMirrors[1] = new ImageIcon(dir + 
+            "PurpleMirrorRight.JPG").getImage();
+        purpleMirrors[2] = new ImageIcon(dir + 
+            "PurpleMirrorDown.JPG").getImage();
+        purpleMirrors[3] = new ImageIcon(dir + 
+            "PurpleMirrorLeft.JPG").getImage();
+
         redLasers = new Image[4];
-        redLasers[0] = new ImageIcon(dir + "LaserUp.JPG").getImage();
-        redLasers[1] = new ImageIcon(dir + "LaserRight.JPG").getImage();
-        redLasers[2] = new ImageIcon(dir + "LaserDown.JPG").getImage();
-        redLasers[3] = new ImageIcon(dir + "LaserLeft.JPG").getImage();
-        
+        redLasers[0] = new ImageIcon(dir + 
+            "LaserUp.JPG").getImage();
+        redLasers[1] = new ImageIcon(dir + 
+            "LaserRight.JPG").getImage();
+        redLasers[2] = new ImageIcon(dir + 
+            "LaserDown.JPG").getImage();
+        redLasers[3] = new ImageIcon(dir + 
+            "LaserLeft.JPG").getImage();
+
         //set window to correct size
-        Dimension size = new Dimension(board.getWidth(null), board.getHeight(null));
+        Dimension size = new Dimension(board.getWidth(null), 
+                board.getHeight(null));
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
@@ -128,7 +141,7 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
     {
         //this is what gets called when the fire button is pressed
         draw = (mirrorPoints[3] == locations.locationPoints[4][3] && 
-        ourLaser == redLasers[0] && ourMovablePurple == purpleMirrors[1]);
+            ourLaser == redLasers[0] && ourMovablePurple == purpleMirrors[1]);
         repaint();
     }
 
@@ -153,7 +166,7 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         //if clicked on movable mirror rotate it
         else if (e.getX() >= x && e.getX() <= x + MIRROR_SIZE &&
         e.getY() >= y && e.getY() <= y + MIRROR_SIZE) {
-            
+
             if (ourMovablePurple == purpleQuestionMark || 
             ourMovablePurple == purpleMirrors[3]) {
                 ourMovablePurple = purpleMirrors[0];
@@ -178,14 +191,14 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
         if (dragging) {
             //find drop location for where mouse was released
             Point drop = locations.getDropPoint(e.getX(), e.getY());
-            
+
             //if not within board put on side panel
             if (drop == null) {
                 x = 602;
                 y = 75;
                 mirrorPoints[3] = sidePanel;
             }
-            
+
             //if within board and not over another piece, place it
             else if (drop != mirrorPoints[0] && drop != mirrorPoints[1] && 
             drop != mirrorPoints[2]) {
@@ -193,13 +206,13 @@ public class BeginnerBoardPanel extends JPanel implements MouseListener, MouseMo
                 y = drop.y;
                 mirrorPoints[3] = drop;
             }
-            
+
             //if over another piece leave it where it came from
             else {
                 x = mirrorPoints[3].x;
                 y = mirrorPoints[3].y;
             }
-            
+
             repaint();
         }
     }
